53af3c977e4d63686b3ccc0dcf7f0ecd1a2f51ee
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-08 21:23:15 +0200

    Add Item class

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,14 +1,16 @@
+require_relative './item'
 class Genre
-  attr_accessor :items
+  attr_reader :id, :items
+  attr_accessor :name
 
-  def initialize(name, _genre)
+  def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
     @items = []
   end
 
   def add_item(item)
-    @items << item
+    @items.push(item)
     item.add_genre(self)
   end
 end
