68d72727350434b1c869feeaccc91cc2cdf6a445
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-08 10:03:30 +0200

    Add genre class

---
+++ classes/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+  attr_accessor :items
+
+  def initialize(name, genre)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_genre(self)
+  end
+end
