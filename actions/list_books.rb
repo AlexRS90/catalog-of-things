60c01b9f4d03f070f6922968d48a707df139d80c
Juan Carlos <jchernandez827@gmail.com>
2022-02-08 12:42:01 -0500

    Add serialization to add_book, change syntax for list_books put

--- actions/list_books.rb
+++ actions/list_books.rb
@@ -2,7 +2,7 @@ class BookList
   def self.list_books(books)
     puts 'Your library is empty' if books.empty?
     books.each_with_index do |book, idx|
-      puts "#{idx}) Title: \"#{book.name}\", Author: \"#{book.author}\", Cover State: \"#{book.cover_state}\""
+      puts "#{idx}) Title: \"#{book['title']}\", Author: \"#{book['author']}\", Cover State: \"#{book['Cover State']}\""
     end
   end
 end
