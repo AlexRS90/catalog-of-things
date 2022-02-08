0014a75b47b8c36904e4cc2f9ed41c2ffc93eddf
Juan Carlos <jchernandez827@gmail.com>
2022-02-08 11:56:40 -0500

    Add accesso for boook and label classes

--- classes/book.rb
+++ classes/book.rb
@@ -1,8 +1,11 @@
 require_relative 'item'
 
 class Book < Item
-  def initialize(publisher:, cover_state:, **args)
+  attr_accessor :name, :publisher, :cover_state
+
+  def initialize(name:, publisher:, cover_state:, **args)
     super(**args)
+    @name = name
     @publisher = publisher
     @cover_state = cover_state
   end
