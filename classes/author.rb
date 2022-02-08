37d883943a75979b31920900dd6caff317be5196
AlexRS90 <aramosbengy@gmail.com>
2022-02-07 21:04:41 -0600

    Create author class

---
+++ classes/author.rb
@@ -0,0 +1,13 @@
+class Author
+  def initialize(first_name:, last_name:)
+    @id = Random.rand(1..10_000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
+  end
+end
