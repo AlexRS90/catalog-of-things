366ff5c0cf28756930f8011e1c6fb536071ea0df
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-09 21:32:29 +0200

    Remove genres json file

--- actions/music_album.rb
+++ actions/music_album.rb
@@ -4,28 +4,6 @@ require_relative '../classes/item'
 
 module MusicAlbum
   def self.create_music(album)
-    on_spotify = nil
-    while on_spotify != 'y' && on_spotify != 'n'
-      print 'On Spotify? [y/n]: '
-      on_spotify = gets.chomp
-      case on_spotify
-      when 'y'
-        true
-      when 'n'
-        false
-      else
-        puts 'invalid input'
-      end
-    end
-    print 'Published Date: '
-    published_date = gets.chomp
-    print 'Published Genre: '
-    genre = gets.chomp
-    print 'Published Author: '
-    author = gets.chomp
-    print 'Published Label: '
-    label = gets.chomp
-
     music_instance = Music.new(on_spotify: on_spotify, publish_date: published_date, genre: genre, author: author,
                                label: label)
     music_album = {
