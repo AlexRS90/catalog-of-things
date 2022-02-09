128eb00cbef7b62beef41d919f90c77efcc32398
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-09 22:35:10 +0200

    Fix linter errors

--- actions/instance_creator.rb
+++ actions/instance_creator.rb
@@ -5,7 +5,7 @@ require './classes/label'
 class InstanceCreator
   def self.genre_instance(item_name)
     genre = Genre.new(name: item_name)
-    { 'name' => genre.name}
+    { 'name' => genre.name }
   end
 
   def self.author_instance(item_author_first, item_author_last)
