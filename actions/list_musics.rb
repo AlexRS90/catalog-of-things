f14561bde901d9ff8f0369754cb1d27d3e5348f5
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-09 22:29:36 +0200

    Display genre name

--- actions/list_musics.rb
+++ actions/list_musics.rb
@@ -2,7 +2,7 @@ class MusicList
   def self.list_music_albums(musics)
     puts 'Your musics is empty' if musics.empty?
     musics.each do |music|
-      puts "Id: #{music['id']}), class: #{music['json_class']}, On Spotify: #{music['on_spotify']}"
+      puts "Id: #{music['id']}), class: #{music['class']}, On Spotify: #{music['on_spotify']}, Genre: #{music['genre']['name']}}"
     end
   end
 end
