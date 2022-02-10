163cfa27a442ff6327e57b1fee3b73d92ceb5c11
Juan Carlos <jchernandez827@gmail.com>
2022-02-10 16:17:52 -0500

    List music now shows name

--- actions/list_musics.rb
+++ actions/list_musics.rb
@@ -3,8 +3,8 @@ class MusicList
     puts
     puts 'Your musics is empty' if musics.empty?
     musics.each_with_index do |music, idx|
-      print "#{idx + 1}) Id: #{music['id']}), Class: #{music['class']}"
-      print " On Spotify: #{music['on_spotify']}, Genre: #{music['genre']['name']}}"
+      print "#{idx + 1}) Title: #{music['title']},"
+      print " Genre: #{music['genre']['name']}, On Spotify: #{music['on_spotify']}"
       puts
     end
     puts
