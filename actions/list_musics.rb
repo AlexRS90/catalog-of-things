693f687cb7ba7a7252c60f0066e483f5b419fba9
Juan Carlos <jchernandez827@gmail.com>
2022-02-10 15:56:01 -0500

    Update lists

--- actions/list_musics.rb
+++ actions/list_musics.rb
@@ -1,11 +1,12 @@
 class MusicList
   def self.list_music_albums(musics)
+    puts
     puts 'Your musics is empty' if musics.empty?
-    musics.each do |music|
-      puts "Id: #{music['id']}),
-            class: #{music['class']},
-            On Spotify: #{music['on_spotify']},
-            Genre: #{music['genre']['name']}}"
+    musics.each_with_index do |music, idx|
+      print "#{idx + 1}) Id: #{music['id']}), Class: #{music['class']}"
+      print " On Spotify: #{music['on_spotify']}, Genre: #{music['genre']['name']}}"
+      puts
     end
+    puts
   end
 end
