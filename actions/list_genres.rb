fc786561294aa2192ac623b2e6c67c6c8e4a3a47
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-10 16:57:39 +0200

    Add method for listing genres

--- actions/list_genres.rb
+++ actions/list_genres.rb
@@ -1,8 +1,21 @@
 class GenreList
-  def self.list_all_genres(genres)
-    puts 'Your genres is empty' if genres.empty?
-    genres.each_with_index do |genre, idx|
-      puts "#{idx}) class: #{genre['class']}, Name: #{genre['name']}"
+  def self.list_all_genres(games, books, musics)
+    puts 'Your genres is empty' if games.empty? && books.empty? && musics.empty?
+    puts
+    puts "Game's genres:"
+    games.each_with_index do |game, idx|
+      puts "#{idx + 1}) #{game['genre']['name']}"
     end
+    puts
+    puts "Book's genres:"
+    books.each_with_index do |book, idx|
+      puts "#{idx + 1}) #{book['genre']['name']}"
+    end
+    puts
+    puts 'Album genres:'
+    musics.each_with_index do |music, idx|
+      puts "#{idx + 1}) #{music['genre']['name']}"
+    end
+    puts ''
   end
 end
