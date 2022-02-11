30c237696c79475b02a1ee508e504e1e6001d2b2
AlexRS90 <aramosbengy@gmail.com>
2022-02-10 21:06:32 -0600

    Add + 1 to start the numbers by 1 instead of 0

--- actions/list_books.rb
+++ actions/list_books.rb
@@ -3,7 +3,7 @@ class BookList
     puts
     puts 'Your library is empty' if books.empty?
     books.each_with_index do |book, idx|
-      print "#{idx}) Title: \"#{book['title']}\", Author: \"#{book['author']['firstname']}"
+      print "#{idx + 1}) Title: \"#{book['title']}\", Author: \"#{book['author']['firstname']}"
       print " #{book['author']['lastname']}\", Cover State: \"#{book['Cover State']}\""
       puts
     end
