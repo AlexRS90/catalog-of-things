276c6a1a4215a4abdc1243025dd09073c9faae80
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-02-11 11:44:39 +0200

    Ask the user to use date format on last updated input

--- inputs/game_inputs.rb
+++ inputs/game_inputs.rb
@@ -3,7 +3,7 @@ class GameInput
     puts
     print 'Is the game multiplayer? [y/n]: '
     multiplayer = gets.chomp.downcase
-    print 'When was the last time you played?: '
+    print 'When was the last date you played?(YY/MM/DD): '
     game_last_played = gets.chomp
 
     [multiplayer, game_last_played]
