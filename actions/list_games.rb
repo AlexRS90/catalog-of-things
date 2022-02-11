7d3b8a50ff672403206d2b7c8b6d72ff21b8bf1b
AlexRS90 <aramosbengy@gmail.com>
2022-02-10 21:06:52 -0600

    Add + 1 to start the numbers by 1 instead of 0

--- actions/list_games.rb
+++ actions/list_games.rb
@@ -3,8 +3,9 @@ class GameList
     puts
     puts 'Your collection is empty' if games.empty?
     games.each_with_index do |game, idx|
-      print "#{idx}) Title: #{game['title']}, Gender: #{game['genre']['name']}, Author: #{game['author']['firstname']} "
-      print "#{game['author']['lastname']}, Multiplayer: #{game['multiplayer']}"
+      print "#{idx + 1}) Title: #{game['title']}, Gender: #{game['genre']['name']}, "
+      print "Author: #{game['author']['firstname']} "
+      print "#{game['author']['lastname']}, Multiplayer: #{game['multiplayer'] == true ? 'Yes' : 'No'}"
       puts
     end
     puts
